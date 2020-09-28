b8ce424458b37205178a261ea7ad11b8f90f10cc
Jorge Becker <a01027251@itesm.mx>
2020-09-28 11:55:09 -0500

    Ejercicios 1 y 2 terminados

--- Ejercicios 1/Code/mas poblacion.py
+++ Ejercicios 1/Code/mas poblacion.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
-population = pd.read_csv("TextFiles/populationbycountry19802010millions.csv")
+population = pd.read_csv("/Users/jorgebecker/Desktop/A01027251-Database/Ejercicios 1/TextFiles/populationbycountry19802010millions.csv")
 
 
 clean=population[["Country","2010"]]
 
-clean["2010"] = clean["2010"].str.replace("NA","").str.replace("--","").astype(float)
+clean['2010'].replace('--','')
 
-print(clean.dtypes)
+
+
 
 top5=clean.sort_values(by='2010',ascending=False)
 
